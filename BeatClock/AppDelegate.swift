048e7bd860be094466553a4c00991269cacd3f36
Julien Mulot <julien.mulot@gmail.com>
2021-04-24 17:49:15 +0200

    Initial Commit

---
+++ BeatClock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BeatClock
+//
+//  Created by Julien Mulot on 24/04/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

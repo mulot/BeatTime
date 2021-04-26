87812c7d652edd0647283f7e584783ec7a4a4384
Julien Mulot <julien.mulot@gmail.com>
2021-04-26 11:51:54 +0200

    Draw Circle and add sub View automatically without Interface Builder

--- BeatClock/AppDelegate.swift
+++ BeatClock/AppDelegate.swift
@@ -9,43 +9,44 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet var window: NSWindow!
+    @IBOutlet var view: NSView!
     @IBOutlet var beatsWindow: NSTextField!
-    @IBOutlet var circleTime: Drawing!
     var timer: Timer!
     var beat: BeatClock!
     var halfSize:CGFloat!
-   
-    let desiredLineWidth:CGFloat = 3
+    
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         beat = BeatClock()
         //beat.isCentiBeat = true
         
         halfSize = min(window.frame.height/2, window.frame.width/2)
-        //circleTime.draw(NSRect(x: window.frame.width/2, y: window.frame.height/2, width: halfSize, height: halfSize))
-        //beatsWindow.stringValue = "@\(beat.beatTime())"
+        let circleView = Drawing(frame: NSRect(x: window.frame.width/2 - (halfSize/2), y: window.frame.height/2 - (halfSize/2), width: halfSize, height: halfSize))
+        circleView.isHidden = false
+        view.addSubview(circleView)
         
-        //timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self]timer in beatsWindow.stringValue = "@\(self.beat.beatTime())"}
+        beatsWindow.stringValue = "@\(beat.beatTime())"
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self]timer in beatsWindow.stringValue = "@\(self.beat.beatTime())"}
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
 class Drawing: NSView {
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
-
+        let lineWidth:CGFloat = 5
         //let fillColor = NSColor.red
         let fillColor = NSColor.clear
         let lineColor = NSColor.blue
         let path = NSBezierPath(ovalIn: dirtyRect)
-        path.lineWidth = 3
+        path.lineWidth = lineWidth
         lineColor.setStroke()
         fillColor.setFill()
         path.fill()

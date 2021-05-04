f57e074015554454f5f910e5a443aeeec6d79e7c
Julien Mulot <julien.mulot@gmail.com>
2021-05-04 14:37:24 +0200

    Transform the circle Line path to a fill Path Draw gradient for the time circle

--- BeatClock/ViewDraw.swift
+++ BeatClock/ViewDraw.swift
@@ -22,24 +22,31 @@ class DrawingArcCircle: NSView {
         
         context.saveGState()
         context.setLineWidth(lineWidth)
-        context.setStrokeColor(lineColor)
         if (isShadow) {
             context.setShadow(offset: CGSize(width: 2, height: -2), blur: 3)
         }
         context.beginPath()
         if (arcFrag != 1000) {
             context.addArc(center: CGPoint(x: dirtyRect.width/2, y: dirtyRect.height/2), radius: (dirtyRect.width/2 - (lineWidth/2)), startAngle: CGFloat(Double.pi/2), endAngle: arcLength, clockwise: true)
+            context.replacePathWithStrokedPath()
+            context.clip()
+            let colorSpace = CGColorSpaceCreateDeviceRGB()
+            let startColor = NSColor.blue
+            guard let startColorComponents = startColor.cgColor.components else { return }
+            let endColor = NSColor.yellow
+            guard let endColorComponents = endColor.cgColor.components else { return }
+            let colorComponents: [CGFloat] = [startColorComponents[0], startColorComponents[1], startColorComponents[2], startColorComponents[3], endColorComponents[0], endColorComponents[1], endColorComponents[2], endColorComponents[3]]
+            let locations:[CGFloat] = [0.0, 1.0]
+            guard let gradient  = CGGradient(colorSpace: colorSpace, colorComponents: colorComponents, locations: locations, count: 2) else { return }
+            let startPoint = CGPoint(x: 0, y: self.bounds.height)
+            let endPoint = CGPoint(x: self.bounds.width,y: self.bounds.height)
+            context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: CGGradientDrawingOptions(rawValue: UInt32(0)))
         }
         else {
             context.addArc(center: CGPoint(x: dirtyRect.width/2, y: dirtyRect.height/2), radius: (dirtyRect.width/2 - (lineWidth/2)), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi*2), clockwise: true)
+            context.setStrokeColor(lineColor)
+            context.strokePath()
         }
-        //context.drawLinearGradient(<#T##gradient: CGGradient##CGGradient#>, start: <#T##CGPoint#>, end: <#T##CGPoint#>, options: <#T##CGGradientDrawingOptions#>)
-        /*
-        if let gradient = NSGradient(starting: NSColor.yellow, ending: NSColor.blue) {
-            gradient.draw(in: context.path, angle: 90)
-        }
-        */
-        context.strokePath()
         context.restoreGState()
     }
 }

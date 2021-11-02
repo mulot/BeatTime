35e9ca2297fa2338803f760a87cf815fb9873d01
Julien Mulot <julien.mulot@gmail.com>
2021-11-02 22:00:12 +0100

    Cancel beat time text foreground color in Widget to let the System color to handle dark mode

--- Widget/BeatTimeWidget.swift
+++ Widget/BeatTimeWidget.swift
@@ -70,7 +70,6 @@ struct BeatTimeWidgetEntryView : View {
                 .gradientForeground(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient])
             Text("@" + entry.beat)
                 .font(.title.bold())
-                .foregroundColor(.startGradient)
         }
         /*
          ZStack {

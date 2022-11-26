6b698cfaa886dfab76a420c5cbd1f2d1b70eb441
Julien Mulot <julien.mulot@gmail.com>
2022-11-26 15:56:30 +0100

    Fix gradientPosition funk of BeaTimeView class with a negative angle after remove the negative return of the hoursOffsetWithGMT function Create functions dedicated to each Widget

--- Widget/BeatTimeWidget.swift
+++ Widget/BeatTimeWidget.swift
@@ -54,125 +54,151 @@ struct BeatEntry: TimelineEntry {
     let beat: String
 }
 
-struct BeatTimeWidgetEntryView : View {
+#if os(watchOS) || os(iOS)
+struct InlineWidget : View {
     var entry: BeatTimeProvider.Entry
     var accentColor: Color = .orange
     
-    @Environment(\.widgetFamily) var family
-    //@Environment(\.widgetRenderingMode) var widgetRenderingMode
+    var body: some View {
+        if #available(iOSApplicationExtension 16.0, *) {
+            ZStack {
+                Text("@\(entry.beat) .beats")
+                    .tint(accentColor)
+            }
+        }
+    }
+}
+#endif
+
+struct RingProgressWidgetSystem : View {
+    var entry: BeatTimeProvider.Entry
+    var accentColor: Color = .orange
+    var lineWidth:CGFloat = 10
     
     var body: some View {
-        switch family {
-        case .systemSmall:
-            ZStack {
-                RingProgressView(arcFrag: 999, lineWidth: 10)
-                    .foregroundColor(.circleLine)
-                RingProgressView(arcFrag: Double(entry.beat)!, lineWidth: 10)
-                    .gradientLinear(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient])
-                Text("@" + entry.beat)
-                    .font(.title.bold())
-            }
-        case .systemMedium:
-            ZStack {
-                RingProgressView(arcFrag: 999, lineWidth: 10)
-                    .foregroundColor(.circleLine)
-                RingProgressView(arcFrag: Double(entry.beat)!, lineWidth: 10)
-                    .gradientLinear(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient])
-                Text("@" + entry.beat)
-                    .font(.title.bold())
-            }
-        case .systemLarge:
-            ZStack {
-                RingProgressView(arcFrag: 999, lineWidth: 20)
-                    .foregroundColor(.circleLine)
-                RingProgressView(arcFrag: Double(entry.beat)!, lineWidth: 20)
-                    .gradientLinear(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient])
-                Text("@" + entry.beat)
-                    .font(.title.bold())
-            }
-        case .systemExtraLarge:
-            ZStack {
-                RingProgressView(arcFrag: 999, lineWidth: 25)
-                    .foregroundColor(.circleLine)
-                RingProgressView(arcFrag: Double(entry.beat)!, lineWidth: 25)
-                    .gradientLinear(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient])
-                Text("@" + entry.beat)
-                    .font(.title.bold())
-            }
-#if os(watchOS) || os(iOS)
-        case .accessoryCircular:
-            if #available(iOSApplicationExtension 16.0, *) {
-                let nbHour = BeatTime.hoursOffsetWithGMT()
-                //let nbHour = 4
-                let angle = (2 * Double.pi) / 24 * Double(nbHour)
-                let startCircle = UnitPoint(x: 0.5, y: 1)
-                //let endCircle = UnitPoint(x: 0.5, y: 1)
-                //let startGradient = startCircle
-                //let endGradient = endCircle
-                let startGradient = UnitPoint(x: startCircle.x + 0.5*sin(angle), y: startCircle.y - 0.5*(1-cos(angle)))
-                let endGradient =  UnitPoint(x: startCircle.x + 0.5*sin(angle+Double.pi), y: startCircle.y - 0.5*(1-cos(angle+Double.pi)))
-                ZStack {
-                    //ProgressView(value: Double(entry.beat)!/1000) { Text(entry.beat) }
-                    //.progressViewStyle(.circular)
-                    Gauge(value: Float(entry.beat)!/1000) { Text("@") }
-                        .gaugeStyle(.accessoryCircular)
-                        //.tint(accentColor)
-                        .gradientLinear(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient], startPoint: startGradient, endPoint: endGradient)
-                    Text(entry.beat)
-                }
-            }
-        case .accessoryInline:
-            if #available(iOSApplicationExtension 16.0, *) {
-                ZStack {
-                    Text("@\(entry.beat) .beats")
-                        .tint(accentColor)
-                }
-            }
-        case .accessoryRectangular:
-            if #available(iOSApplicationExtension 16.0, *) {
-                let nbHour = BeatTime.hoursOffsetWithGMT()
-                //let nbHour = -4
-                let gmt:String = nbHour > 0 ? "+\(nbHour)" : "\(nbHour)"
-                let index = ((abs(nbHour)/2%6)+5)%6
-                let colors:[Color] = [.mid2Gradient, .endGradient, .mid2Gradient, .midGradient, .startGradient, .midGradient]
-                let gradients:[Color] = [colors[(index)%colors.count], colors[(index+1)%colors.count], colors[(index+2)%colors.count], colors[(index+3)%colors.count], colors[(index+4)%colors.count], colors[(index+5)%colors.count]]
-                ZStack {
-                    ProgressView(value: Double(entry.beat)!/1000) { Text("@\(entry.beat) .beats GMT\(gmt)") }
-                        .progressViewStyle(.linear)
-                        .gradientLinear(colors: gradients, startPoint: .leading, endPoint: .trailing)
-                        //.gradientRadius(colors: gradients, center: centerPoint, startRadius: 10, endRadius: 90)
-                        //.tint(accentColor)
-                }
-            }
-#endif
-#if os(watchOS)
-        case .accessoryCorner:
-            //Gauge(value: Float(entry.beat)!/1000) { Text(entry.beat) }
-            //ProgressView(value: Double(entry.beat)!/1000) { Text(entry.beat) }
-            //.progressViewStyle(.automatic)
-            //Image(systemName: "at.circle")
-            Text(entry.beat)
-                .foregroundColor(accentColor)
+        ZStack {
+            RingProgressView(arcFrag: 999, lineWidth: lineWidth)
+                .foregroundColor(.circleLine)
+            RingProgressView(arcFrag: Double(entry.beat)!, lineWidth: lineWidth)
+                .gradientLinear(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient])
+            Text("@" + entry.beat)
                 .font(.title.bold())
-                .widgetLabel {
-                    ProgressView(value: Double(entry.beat)!/1000)
-                        .tint(accentColor)
-                }
+        }
+    }
+}
+
+#if os(watchOS) || os(iOS)
+struct GaugeWidget : View {
+    var entry: BeatTimeProvider.Entry
+    var accentColor: Color = .orange
+    
+    var body: some View {
+        if #available(iOSApplicationExtension 16.0, *) {
+            let nbHour = BeatTime.hoursOffsetWithGMT()
+            //let nbHour = 4
+            let angle = (2 * Double.pi) / 24 * Double(nbHour)
+            let startCircle = UnitPoint(x: 0.5, y: 1)
+            //let endCircle = UnitPoint(x: 0.5, y: 1)
+            //let startGradient = startCircle
+            //let endGradient = endCircle
+#if os(watchOS)
+            let startGradient = UnitPoint(x: startCircle.x + 0.5*sin(angle), y: startCircle.y - 0.5*(1-cos(angle)))
+            let endGradient =  UnitPoint(x: startCircle.x + 0.5*sin(angle+Double.pi), y: startCircle.y - 0.5*(1-cos(angle+Double.pi)))
 #endif
-        default:
             ZStack {
-                RingProgressView(arcFrag: 999, lineWidth: 10)
-                    .foregroundColor(.circleLine)
-                RingProgressView(arcFrag: Double(entry.beat)!, lineWidth: 10)
-                    .gradientLinear(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient])
-                Text("@" + entry.beat)
-                    .font(.title.bold())
+                //ProgressView(value: Double(entry.beat)!/1000) { Text(entry.beat) }
+                //.progressViewStyle(.circular)
+                Gauge(value: Float(entry.beat)!/1000) { Text("@") }
+                    .gaugeStyle(.accessoryCircular)
+                //.tint(accentColor)
+#if os(watchOS)
+                    .gradientLinear(colors: [.startGradient, .midGradient, .mid2Gradient, .endGradient], startPoint: startGradient, endPoint: endGradient)
+#endif
+#if os(iOS)
+                    .tint(accentColor)
+#endif
+                Text(entry.beat)
+            }
+        }
+    }
+}
+#endif
+
+struct RectangularsWidget : View {
+    var entry: BeatTimeProvider.Entry
+    var accentColor: Color = .orange
+    
+    var body: some View {
+        if #available(iOSApplicationExtension 16.0, *) {
+            let nbHour = BeatTime.hoursOffsetWithGMT()
+            //let nbHour = -4
+            let gmt:String = nbHour > 0 ? "+\(nbHour)" : "\(nbHour)"
+            let index = ((abs(nbHour)/2%6)+5)%6
+            let colors:[Color] = [.mid2Gradient, .endGradient, .mid2Gradient, .midGradient, .startGradient, .midGradient]
+#if os(watchOS)
+            let gradients:[Color] = [colors[(index)%colors.count], colors[(index+1)%colors.count], colors[(index+2)%colors.count], colors[(index+3)%colors.count], colors[(index+4)%colors.count], colors[(index+5)%colors.count]]
+#endif
+            ZStack {
+                ProgressView(value: Double(entry.beat)!/1000) { Text("@\(entry.beat) .beats GMT\(gmt)") }
+                    .progressViewStyle(.linear)
+#if os(watchOS)
+                    .gradientLinear(colors: gradients, startPoint: .leading, endPoint: .trailing)
+#endif
+#if os(iOS)
+                    .tint(accentColor)
+#endif
+                //.gradientRadius(colors: gradients, center: centerPoint, startRadius: 10, endRadius: 90)
+                //.tint(accentColor)
             }
         }
     }
 }
 
+#if os(watchOS)
+struct CornerWidget : View {
+    var entry: BeatTimeProvider.Entry
+    var accentColor: Color = .orange
+    
+    var body: some View {
+        //Gauge(value: Float(entry.beat)!/1000) { Text(entry.beat) }
+        //ProgressView(value: Double(entry.beat)!/1000) { Text(entry.beat) }
+        //.progressViewStyle(.automatic)
+        //Image(systemName: "at.circle")
+        Text(entry.beat)
+            .foregroundColor(accentColor)
+            .font(.title.bold())
+            .widgetLabel {
+                ProgressView(value: Double(entry.beat)!/1000)
+                    .tint(accentColor)
+            }
+    }
+}
+#endif
 
+struct BeatTimeWidgetEntryView : View {
+    var entry: BeatTimeProvider.Entry
+    
+    @Environment(\.widgetFamily) var family
+    
+    @ViewBuilder
+    var body: some View {
+        switch family {
+        case .systemSmall: RingProgressWidgetSystem(entry: entry)
+        case .systemMedium: RingProgressWidgetSystem(entry: entry)
+        case .systemLarge: RingProgressWidgetSystem(entry: entry, lineWidth: 20)
+        case .systemExtraLarge: RingProgressWidgetSystem(entry: entry, lineWidth: 25)
+#if os(watchOS) || os(iOS)
+        case .accessoryCircular: GaugeWidget(entry: entry)
+        case .accessoryInline: InlineWidget(entry: entry)
+        case .accessoryRectangular: RectangularsWidget(entry: entry)
+#endif
+#if os(watchOS)
+        case .accessoryCorner: CornerWidget(entry: entry)
+#endif
+        default: RingProgressWidgetSystem(entry: entry)
+        }
+    }
+}
 
 @main
 struct BeatTimeWidget: Widget {
@@ -196,16 +222,11 @@ struct BeatTimeWidget: Widget {
     }()
     
     var body: some WidgetConfiguration {
-        //if #available(iOSApplicationExtension 16.0, *) {
         StaticConfiguration(
             kind: "org.mulot.beattime.BeatTimeWidget",
             provider: BeatTimeProvider()
         ) { entry in
             BeatTimeWidgetEntryView(entry: entry)
-            //.frame(maxWidth: .infinity, maxHeight: .infinity)
-            //.background(Color("WidgetBackground"))
-            //.background(Color.black)
-            //.foregroundColor(.gray)
         }
         .configurationDisplayName("BeatTime")
         .description("Swatch Internet Time aka .beat time")
